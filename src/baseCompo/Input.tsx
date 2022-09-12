ac1f2143b460b0f972e9a172e8b4447b1a71b964
bhoopendra <bhaskarbhoopendra8@gmail.com>
2022-09-12 20:13:28 +0530

    change in input component

--- src/baseCompo/Input.tsx
+++ src/baseCompo/Input.tsx
@@ -1,15 +1,24 @@
-import "../baseCompo/styles.css"
+import "../baseCompo/styles.css";
 
-export default function InputComponent({ type , label, fn}: any){
-  console.log({fn})
- return (
+export default function InputComponent({ type, label, fn }: any) {
+  console.log({ fn });
+  return (
     <div className="d-flex flex-column m-20 p-10">
       <label className="m-10 p-2 lbl" htmlFor="name">
         {label}
       </label>
-      <input className='p-1 m-3' style={{border:"none", borderBottom:"0.5px solid black"}} type={type} onChange={fn}  />
+      <input
+        className="p-1 m-3"
+        style={{ border: "none", borderBottom: "0.5px solid black" }}
+        type={type}
+        onChange={fn}
+      />
+      <input
+        className="p-1 m-3"
+        style={{ border: "none", borderBottom: "0.5px solid black" }}
+        type={type}
+        onChange={fn}
+      />
     </div>
   );
 }
-
-

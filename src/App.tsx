101ea16d43f7f730cf9f5436c9b5739280a60a72
LORDSAKSHAM <saksham.mishraa79@gmail.com>
2022-09-14 22:29:35 +0530

    Signin page fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Forms from "./pages/Forms";
 // import { Navbar } from "./components/Navbar";
 import { Home } from "./pages/Home";
-import { Sign } from "./pages/Sign";
+import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<Sign />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="/forms" element={<Forms />} />
       <Route path="/signin" element={<Signin />} />
     </Routes>

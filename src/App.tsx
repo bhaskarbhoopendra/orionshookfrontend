b6d3230c95d8891a4445a93d13009b931253d68f
LORDSAKSHAM <saksham.mishraa79@gmail.com>
2022-09-20 16:09:58 +0530

    sidebar initial

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,30 @@ import Forms from "./pages/Forms";
 import { Home } from "./pages/Home";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
+
+import { Row, Col } from "react-bootstrap";
 import { SideBar } from "./basecomponents/SideBar";
+import Attendance from "./pages/Attendance";
 
 function App() {
   return (
+//     <Row>
+//       <Col xl={3}>
+//         <SideBar/>
+// 
+//       </Col>
+//       <Col xl={9}>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/forms" element={<Forms />} />
       <Route path="/signin" element={<Signin />} />
-      <Route path="/sidebar" element={<SideBar />} />
+      <Route path="/attendance" element={<Attendance />} />
+      <Route path="/signup" element={<SideBar />} />
+    
     </Routes>
+    // </Col>
+    // </Row>
   );
 }
 
--- src/basecomponents/SideBar.tsx
+++ src/basecomponents/SideBar.tsx
@@ -1,10 +1,23 @@
 import SidebarItem from "./sideBarItem";
 import items from "../Data/sideBar.json";
+import { Card } from "react-bootstrap";
 
 export function SideBar() {
   return (
     <>
       <div className="main">
+        <div>
+        <Card style={{ width: '16.2rem', borderRadius:"0px" }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+        </div>
         <div className="sidebar">
           {items.map((item, index) => (
             <SidebarItem key={index} item={item} />
